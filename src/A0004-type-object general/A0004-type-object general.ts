817a467d2e2bb250766f6e0fabe7953ea92be5e8
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-02-12 21:41:40 -0300

    Type object general

--- src/A0004-type-object general/A0004-type-object general.ts
+++ src/A0004-type-object general/A0004-type-object general.ts
@@ -1,28 +1,9 @@
-/* Eslit-disable*/
-
-/* Tipos basicos*/
-let nome: string = 'luiz';
-let idade: number = 0b1010; //3, 1.65, 0b1010. etcc
-let adulto: boolean = true;
-let simbolo: symbol = Symbol('qualquer-symbol');
-// let big: biginit = 10n; //biginit
-
-// Arrays
-let arrayNumbers: Array<number> = [1, 2, 3];
-let arrayNumbers2: number[] = [1, 2, 3];
-let arrayString: Array<String> = ['a', 'b', 'c'];
-let arrayString2: string[] = ['a', 'b', 'c'];
-
-//Objetos
-let pessoa: { nome: string; idade: number; adulto?: boolean } = {
-  idade: 30,
-  nome: 'lucas',
-  adulto: true,
+const objetoA: {
+  readonly chaveA: string; //Não posso alterar o valor dessa chave por conta do readonly
+  chaveB: string;
+} = {
+  chaveA: 'Valor A',
+  chaveB: 'Valor B',
 };
-
-//Functions
-function soma(x: number, y: number) {
-  return x + y;
-}
-
-const soma2: (x: number, y: number) => number = (x, y) => x + y;
+objetoA.chaveB = 'outroValor';
+~console.log(objetoA);
