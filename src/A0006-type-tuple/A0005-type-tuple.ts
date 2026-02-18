84338585a4f624e74fcd0f807be2680c2cfc746b
Lucas Eduardo de Melo <136135204+lucaseduardo5855@users.noreply.github.com>
2026-02-17 22:41:43 -0300

    Tuple

---
+++ src/A0006-type-tuple/A0005-type-tuple.ts
@@ -0,0 +1,20 @@
+//Tupla -> array de tamanho fixo
+const dadosCliente1: readonly [number, string] = [1, 'Luiz'];
+const dadosCliente2: [number, string, string] = [10, 'Bacana', 'Legal'];
+const dadosCliente3: [number, string, string?] = [12, 'tresh'];
+const dadosCliente4: [number, string, ...string[]] = [12, 'tresh', 'miranda'];
+
+//dadosCliente1[0] = 100;
+//dadosCliente1[1] = 'legal';
+ 
+console.log(dadosCliente1)
+console.log(dadosCliente2)
+console.log(dadosCliente3)
+console.log(dadosCliente4)
+
+//Readonly Array 
+const array1: readonly string[] = ['fernando', 'Lucas'];
+const array2: ReadonlyArray<string> = ['Gabriel', 'Leo'];
+
+console.log(array1);
+console.log(array2);
