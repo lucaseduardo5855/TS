b7cb9b942ec74ceb56d4279c60cac188d744ff81
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-03-04 19:33:38 -0300

    Associação exercicio

--- src/A0030-Associacao/A0030-Associacao.ts
+++ src/A0030-Associacao/A0030-Associacao.ts
@@ -1,25 +1,63 @@
+//Escritor
 export class Escritor{
-  private _ferramenta = Ferramenta | null = null;
+  private _ferramenta: Ferramenta | null = null; //Ferramenta não depende de escritor então nula ou ela.
 
   constructor(private _nome: string){}
 
-  getNome():string{
+  get nome():string{
     return this._nome;
   }
 
-  setFerramenta(ferramenta: Ferramenta){
-    this._ferramenta = ferramenta;
+  set ferramenta(ferramenta: Ferramenta | null){
+     this._ferramenta = ferramenta;
+  }
+
+  get ferramenta(): Ferramenta | null{
+    return this._ferramenta;
+  }
+
+  escrever():void{
+    if(this.ferramenta === null) {
+      console.log('Não temos ferramentas para escrever ou digitar!')
+      return;
+    }
+    this.ferramenta.escrever();
   }
 }
 
 
+//Ferramenta
 export abstract class Ferramenta{
-  constructor(nome: string){}
-
-  getNome():string{
-    
+  constructor(private _nome: string){}
+  
+  get nome():string{
+    return this._nome;
   }
 
-  abstract escrever():void;
+  abstract escrever():void; //mnétodo abstrato
 }
 
+
+//Extensões abstratas
+class Caneta extends Ferramenta{
+  escrever(): void {
+    console.log(`${this.nome} está escrevendo...`)
+  }
+}
+
+
+class MaquinaEs extends Ferramenta{
+  escrever(): void {
+    console.log(`${this.nome} está digitando, tac, tac, tac...`)
+  }
+}
+
+
+//variaveis
+const escritor = new Escritor('Lucas');
+const caneta = new Caneta('bic');
+const maquina = new MaquinaEs('Maquina Escrever');
+
+
+escritor.ferramenta = maquina;
+escritor.escrever();
