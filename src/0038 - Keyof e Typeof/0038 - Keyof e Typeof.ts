6499175c574c43191378ba22b6e1179f0b7929b5
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-03-09 23:55:41 -0300

    ChaveEmTypes

--- src/0038 - Keyof e Typeof/0038 - Keyof e Typeof.ts
+++ src/0038 - Keyof e Typeof/0038 - Keyof e Typeof.ts
@@ -1,21 +1,17 @@
 //Exemplo: Traduzir Cores
-type CoresObj = typeof coresObj;
-type CoresChaves = keyof CoresObj;
 
-const coresObj = {
-  rosa: 'pink',
-  verde: 'green',
-  amarelo: 'yellow',
-  roxo: 'purple'
-};
+type coresObj = typeof ObjCores;
+type ChaveCores = keyof coresObj;
 
-function traduzirCores(
-  cor: CoresChaves,
-  cores: typeof coresObj,
-) {
-  return cores[cor];
+const ObjCores = {
+  vermelho: 'red',
+  roxo: 'purple',
+  amarelo: 'yellow'
 }
 
-console.log(traduzirCores('rosa', coresObj));
-console.log(traduzirCores('amarelo', coresObj));
-console.log(traduzirCores('roxo', coresObj));
+function traduzirCor(cor: ChaveCores, cores: coresObj){
+  return cores[cor]
+}
+
+
+console.log(traduzirCor('roxo', ObjCores));
---
+++ src/0039 - ChaveEmTipos/0039 - ChaveEmTipos.ts
@@ -0,0 +1,18 @@
+type Veiculo = {
+  marca: string;
+  ano: string;
+};
+
+type Car = {
+  brand: Veiculo['marca'];
+  year: Veiculo['ano'];
+  name: string;
+};
+
+const carro: Car = {
+  brand: 'ford',
+  year: '2026',
+  name: 'civic',
+};
+
+console.log(carro)
