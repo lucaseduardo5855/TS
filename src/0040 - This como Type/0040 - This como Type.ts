0b3f667f3ad67bc974af4d19c102869e98ea5cf7
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-03-10 20:46:22 -0300

    Usando This como Type

--- src/0040 - This como Type/0040 - This como Type.ts
+++ src/0040 - This como Type/0040 - This como Type.ts
@@ -25,3 +25,28 @@ export class Calculadora{
 const calc = new Calculadora(10);
 calc.add(10).mult(20);
 console.log(calc)
+
+
+
+//Builder - GoF
+export class RequestBuilder{
+  private method: 'get' |  'post' |  null = null;
+  private url: string |  null = null;
+
+  setMethod(method: 'get' |  'post'):this{
+    this.method = method;
+    return this;
+  }
+
+  setUrl(url: string): this{
+    this.url = url;
+    return this;
+  }
+
+  send():void{
+    console.log(`Enviando dados via ${this.method} para ${this.url}`)
+  }
+}
+
+const request = new RequestBuilder();
+request.setUrl('http://www.google.com').setMethod('post').send();
