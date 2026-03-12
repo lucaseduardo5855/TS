01298e62be169d47c6476a3067e4238abf3c4a2e
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-03-11 23:41:06 -0300

    Generics U e T, interface e Type

---
+++ src/0045-Generics 3/0045-Generics 3.ts
@@ -0,0 +1,31 @@
+interface PessoaProtocolo<T = string, U = number>{
+  nome: T;
+  sobrenome: T;
+  idade: U
+}
+
+type PessoaProtocolo2<T = string, U = Number> {
+  nome: T,
+  sobrenome: T,
+  idade: U
+}
+
+const aluno: PessoaProtocolo<string, number> = {
+  nome: 'lucas',
+  sobrenome: 'eduardo',
+  idade: 22
+};
+
+const aluno2: PessoaProtocolo<number, number> = {
+  nome: 123,
+  sobrenome: 12323,
+  idade: 22
+};
+
+const aluno3: PessoaProtocolo2 = {
+  nome: 'lucas',
+  sobrenome: 'eduardo',
+  idade: 22
+};
+
+console.log(aluno, aluno2, aluno3)
