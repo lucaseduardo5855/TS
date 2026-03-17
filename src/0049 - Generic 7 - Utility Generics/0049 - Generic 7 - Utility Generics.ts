0541b0b0bc9d85e413064b2a466855e7164ebd0c
Lucas Eduardo <lucas.eduardo5855@gmail.com>
2026-03-17 20:30:03 -0300

    Type Record, Required, Partial, Readonly, Pick, Extract e Excluse

--- src/0049 - Generic 7 - Utility Generics/0049 - Generic 7 - Utility Generics.ts
+++ src/0049 - Generic 7 - Utility Generics/0049 - Generic 7 - Utility Generics.ts
@@ -1,10 +1,69 @@
+//Record
 const objeto1: Record<string, string | number> = {
   nome: 'Lucas',
-  sobrenome: 'eduardo',
-  idade: 30
+  sobrenome: 'Eduardo',
+  idade: 30,
+};
+console.log(objeto1);
+
+type PessoaProtocol = {
+  nome?: string;
+  sobrenome?: string;
+  idade?: number;
+};
+
+//Required
+type PessoaRequired = Required<PessoaProtocol>;
+
+//Partial
+type PessoaPartial = Partial<PessoaRequired>;
+
+//Readonly
+type PessoaReadonly = Readonly<PessoaRequired>;
+
+//Pick
+type PessoaPick = Pick<PessoaRequired, 'nome' | 'sobrenome'>;
+
+const objeto2: PessoaRequired = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
+  idade: 30,
+};
+console.log(objeto2);
+
+//Extract e Exclude
+type ABC = 'A' | 'B' | 'C';
+type CDE = 'C' | 'D' | 'E';
+type TipoExclude = Exclude<ABC, CDE>;
+type TipoExtract = Extract<ABC, CDE>;
+
+//Ex MongoDB
+type AccountMongo = {
+  _id: string;
+  nome: string;
+  idade: number;
+};
+
+//Ex API
+type AccountApi = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & {
+  //Pega todo o AccountMongo e retira o '_id'
+  id: string;
+};
+
+const accountmongo: AccountMongo = {
+  _id: 'qwiuehqweu',
+  nome: 'Lucas',
+  idade: 30,
+};
+
+function mapAccount(accountmongo: AccountMongo): AccountApi {
+  const { _id, ...accountData } = accountmongo;
+  return { ...accountData, id: _id };
 }
 
-console.log(objeto1);
+const accountapi = mapAccount(accountmongo);
+console.log('ABAIXO É A API');
+console.log(accountapi);
 
 //Module Mode
 export default 1;
